ddae67d677513047fc26be3229ac9ce5f2425b0a
Oscar Johnson <ojohns7@tigers.lsu.edu>
2015-02-25 12:23:35 -0600

    minor update
    Markov chain simulator containing derived classes for both discrete-time and continuous-time markov chains and some associated functions

--- MarkovObjects.py
+++ MarkovObjects.py
@@ -11,6 +11,8 @@ from __future__ import division
 import random
 import numpy as np
 import scipy as sp
+from scipy.stats import binom
+
 
 class markov(object):
     """base class for markov chain simulators
@@ -24,7 +26,7 @@ class markov(object):
        Q = Q-matrix
     """    
     
-    def __init__(self, stateSpace = ('A', 'B'), numSims = 1, chainLength = 10, 
+    def __init__(self, stateSpace = ('A', 'C', 'G', 'T'), numSims = 1, chainLength = 10, 
                 matrix = [[0.8, 0.2], [0.3, 0.7]],
                 Q = np.matrix([[-1.916, 0.541, 0.787, 0.588], 
                                [0.148, -1.069, 0.415, 0.506], 
@@ -166,19 +168,42 @@ class dtmarkov(markov):
         print "\n", "the sum of probabilities is: ", sum
         return sum
 
-    # Below are various functions that I will eventually add to this class.
+
+    def mcStateFreqSum(self,state="A"):
+        """
+        Pass this function a list of lists. Each individual list should be the
+        states of a discrete-state Markov chain through time (and all the same 
+        length). It will return a list (freqs) containing the frequency of one state 
+        ("A" by default) across all simulations through time.
+        """
+        freqs = []
+        for i in range(len(self.chain[0])):  # Iterate across time steps
+            stateCount = 0
+            for j in range(len(self.chain)): # Iterate across simulations
+                if self.chain[j][i] == state:
+                    stateCount += 1
+            freqs.extend([float(stateCount)/float(len(self.chain))])
+        return freqs
+
 
     def likelihood(self):
-        """Calculate likelihoods
+        """Calculates likelihood scores given discrete time marcov chains
         """
-     
-            
-    def stateTime(self):
-        """ 
+        likeScoresA = []
+        likeScoresC = []
+        likeScoresG = []
+        likeScoresT = []
+        pList = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
         
-        """
-       
+        for i in pList:
+            likeScoresA.append(binom.pmf(self.chain.count("A"), self.chainLength, i))
+            likeScoresC.append(binom.pmf(self.chain.count("C"), self.chainLength, i))
+            likeScoresG.append(binom.pmf(self.chain.count("G"), self.chainLength, i))
+            likeScoresT.append(binom.pmf(self.chain.count("T"), self.chainLength, i))
 
+        return likeScoresA, likeScoresC, likeScoresG, likeScoresT
+
+            
 class ctmarkov(markov):
     """ Continuous-time Markov chain simulator
         
@@ -188,14 +213,15 @@ class ctmarkov(markov):
         RateMatrix = Q-Matrix of transition probabilities for a continuous markov chain        
     """
     
+    
     def ctmcSim(self):
         """ A continuous-time markov chain simulator. 
         
             Takes the 4-state nucleotide State Space and 
             nucleotide Rate Matrix defined above
         """
-        self.times = []
-        self.chain = []
+        #self.times = []
+        #self.chain = []
         currState = self.discSamp(self.stateSpace,[1.0/len(self.stateSpace) for x in self.stateSpace])
         self.chain.extend(currState)
         
@@ -227,12 +253,25 @@ class ctmarkov(markov):
         #self.margQ = sp.linalg.expm(self.Q*self.chainLength)
         #return self.margQ
 
+    def multSimsCTMC(self):
+        """ runs multiple simulations of the continuous time markov simulator
+    
+            Attributes:
+            numSims = number of simulations to be run. defined in base class
+        """
+        for x in range(self.numSims): 
+            run = self.ctmcSim()
+            self.chain.append(run[0])
+        return self.chain
+
+
 
 # stuff to pass the ctmarkov class in an example
 simulation1 = ctmarkov(stateSpace = ("A", "C", "G", "T"))
 print simulation1
 
 """ IT'S WORKING!!!!
+    This is from a continuous time marcov chain simulator
 (['G', 'C', 'G', 'G', 'C', 'C', 'C', 'A', 'G', 'A', 'G', 'G', 'G'], 
  [1.0503962271454141, 0.57410609394085899, 2.431084419690781, 2.2274541761426785, 
   0.38986908712834706, 0.47031628468991038, 0.89942168665521571, 0.0059148246206891363, 
